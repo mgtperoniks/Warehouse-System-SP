0d1b952cef09be1c61c782c00b537481ab082f51
ppic2 <ppic2@pkshp.com>
2026-07-02 16:29:03 +0700

    temp: add upload debug logging in middleware

--- app/Http/Middleware/WmsWarehouseContextMiddleware.php
+++ app/Http/Middleware/WmsWarehouseContextMiddleware.php
@@ -10,6 +10,28 @@ class WmsWarehouseContextMiddleware
 {
     public function handle(Request $request, Closure $next): Response
     {
+        if (str_contains($request->getPathInfo(), 'upload-file')) {
+            \Illuminate\Support\Facades\Log::info('LIVEWIRE_UPLOAD_DEBUG', [
+                'fullUrl' => $request->fullUrl(),
+                'url' => $request->url(),
+                'host' => $request->getHost(),
+                'getHttpHost' => $request->getHttpHost(),
+                'port' => $request->getPort(),
+                'scheme' => $request->getScheme(),
+                'query' => $request->query(),
+                'headers' => $request->headers->all(),
+                'hasValidSignature' => $request->hasValidSignature(),
+                'app_url' => config('app.url'),
+                'url_root' => url('/'),
+                'server_port' => $request->server('SERVER_PORT'),
+                'server_http_host' => $request->server('HTTP_HOST'),
+                'request_uri' => $request->server('REQUEST_URI'),
+                'query_string' => $request->server('QUERY_STRING'),
+                'signature_param' => $request->query('signature'),
+                'expires_param' => $request->query('expires'),
+            ]);
+        }
+
         if (auth()->check()) {
             $user = auth()->user();
             $activeWarehouseId = session('active_warehouse_id');
