dd47bfed2cdc38fe4e0a5590c4bf01e4865813a2
ppic2 <ppic2@pkshp.com>
2026-07-10 09:03:02 +0700

    fix: register Cropper globally on window object

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap';
 import 'material-symbols/outlined.css';
 import { Html5Qrcode } from 'html5-qrcode';
-import '../../public/assets/js/cropper.min.js';
+import Cropper from '../../public/assets/js/cropper.min.js';
 
 window.Html5Qrcode = Html5Qrcode;
+window.Cropper = Cropper;
